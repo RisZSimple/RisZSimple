3ea4e63fe01e5e682b52c4ded0256afd97445af5
RisZSimple <110978523+RisZSimple@users.noreply.github.com>
2022-08-10 18:14:11 +0700

    Update RisZSimple.py

--- RisZSimple.py
+++ RisZSimple.py
@@ -32,7 +32,7 @@ def run():
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-			print(i +" Sent!!!")
+			print(i +" Misi Paket Anj!!")
 		except:
 			print("[!] Error!!!")
 			
